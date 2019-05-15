56c04ea4318e3c96c3494f94299914f70b243915
Leonardo Bittencourt <leombittencourt@gmail.com>
2019-05-14 21:24:39 -0400

    Commit Customer Persisting

---
+++ src/backoffice/services/customer.service.ts
@@ -0,0 +1,16 @@
+import { Model } from 'mongoose';
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Customer } from '../models/customer.model';
+
+@Injectable()
+export class CustomerService {
+    constructor(@InjectModel('Customer') private readonly model: Model<Customer>) {
+
+    }
+
+    async create(data: Customer): Promise<Customer> {
+        const user = this.model(data);
+        return await user.save();
+    }
+}
