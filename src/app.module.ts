b5f79c4ae7b7486137afa95f26bf370bf7ec4521
Leonardo Bittencourt <leombittencourt@gmail.com>
2019-05-28 21:27:31 -0400

    Add module Store

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { BackofficeModule } from './modules/backoffice/backoffice.module';
+import { StoreModule } from './modules/store/store.module';
 
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb+srv://admin:admin@cluster0-ijztf.mongodb.net/test?retryWrites=true'),
     BackofficeModule,
+    StoreModule,
   ],
   controllers: [],
   providers: []
