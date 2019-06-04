84269fe8de42fa2afffaa86208a2883c65e8efea
Leonardo Bittencourt <leombittencourt@gmail.com>
2019-06-03 20:38:16 -0400

    Adjustes in typeorm settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,22 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BackofficeModule } from './modules/backoffice/backoffice.module';
 import { StoreModule } from './modules/store/store.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb+srv://admin:admin@cluster0-ijztf.mongodb.net/test?retryWrites=true'),
-    // TypeOrmModule.forRoot({
-    //   type: 'mysql',
-    //   host: 'localhost',
-    //   port: 3306,
-    //   username: 'root',
-    //   password: 'mysql',
-    //   database: '7180',
-    //   entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    //   synchronize: true,
-    // }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'mysql',
+      database: '7180',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
     BackofficeModule,
     StoreModule,
   ],
--- src/modules/store/store.module.ts
+++ src/modules/store/store.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from 'src/modules/store/entities/product.entity';
+import { ProductService } from 'src/modules/store/services/product.service';
+import { ProductController } from 'src/modules/store/controllers/product.controller';
 
 @Module({})
-export class StoreModule {}
+export class StoreModule { }
